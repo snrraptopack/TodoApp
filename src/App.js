f4d63ec1e7c0c58446a254c23c723b64f404109d
Amihere Theophilus Junior <snrraptopack@gmail.com>
2023-10-07 12:18:42 +0000

    A todo app to keep track of my todo project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import Screen from "./Screen";
+import React  from "react";
+import Todos from "./Todos";
 
 function App() {
+    const [todoScreen,setTodoScreen] = React.useState(false)
+    const [view,setView] = React.useState(false)
+    const [todoItems,setTodoItems] = React.useState(()=>{
+        return  JSON.parse(localStorage.getItem("mytodo") || [])
+    })
+    const [currentTodo,setCurrentTodo] =React.useState('')
+    const [showModel,setShowModel] =React.useState(false)
+    const [addTools,setAddTools] = React.useState(false)
+    const [currentIndex ,setCurrentIndex]= React.useState(0)
+
+
+    //for setting getting the current input of the todo item being listed
+    function  handleChange(event){
+        const {value} =event.target
+        setCurrentTodo(value)
+    }
+    // a dding items to the todo list
+    function  handleTodo(){
+        setTodoItems(prevItems=>{
+            return [...prevItems,{title:currentTodo,isCompleted:false,tools:[]}]
+        })
+        setCurrentTodo("")
+    }
+
+    localStorage.setItem('mytodo',JSON.stringify(todoItems))
+
+    //for the completed todo tools used
+    function handleComplete(event) {
+        const {value} = event.target
+        setCurrentTodo(value)
+    }
+    function show(i){
+        setShowModel(true)
+        setCurrentIndex(i)
+    }
+
+    function  addTodoTools(){
+        setTodoItems(prevState => {
+            return  prevState.map((items,index)=>{
+                if(index === currentIndex){
+                    let tool= items.tools
+                    return {...items,isCompleted:true, tools:[...tool,currentTodo]}
+                }else{
+                    return  items
+                }
+            })
+        })
+
+        setCurrentTodo("")
+    }
+
+    console.log(todoItems)
+    function  handleDelete(){
+        setTodoItems(prevState=>{
+           return  prevState.filter((items)=>{
+                return items !== prevState[currentIndex]
+            })
+        })
+    }
+    function  close(){
+        setShowModel(false)
+    }
+    function proceed(){
+        setAddTools(true)
+    }
+
+
+    const carStyles= {
+        card:   `w-52 h-36 mx-auto md:mx-3 my-12 flex justify-center items-center shadow-cyan-500/50 shadow-2xl hover:bg-white
+                 transition-colors ease-out delay-75`,
+        cardText:`text-purple-700 text-2xl font-bold`
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="flex min-h-screen flex-col justify-center items-center bg-blue-950 md:flex-row relative ">
+           <div
+               onClick={()=>setTodoScreen(true)}
+               className={`${carStyles.card} ${todoScreen || view? 'hidden': "visible"}`}>
+                <h3 className={carStyles.cardText}>Create a Todo</h3>
+           </div>
+
+            <div
+                onClick={()=>setView(true)}
+                className={`${carStyles.card} ${todoScreen || view? 'hidden': "visible"}`}>
+                <h3 className={carStyles.cardText}>View Todo</h3>
+            </div>
+
+            {todoScreen && <Screen handlechange={handleChange} current={currentTodo} handletodo={handleTodo}/>}
+            {view ? <Todos
+                setshow={show}
+                opencomplete={handleComplete}
+                close={close}
+                add={addTools}
+                proceed={proceed}
+                items={todoItems}
+                show={showModel}
+                current={currentTodo}
+                addtodotools={addTodoTools}
+                delete={handleDelete}
+            /> : todoItems.length && todoScreen?
+                                <div
+                                    onClick={()=>{
+                                        setTodoScreen(false)
+                                        setView(true)
+                                    }}
+                                    className={`bg-fuchsia-400 w-[40%] h-12 fixed top-0 right-0 flex  items-center justify-center  text-slate-50 font-bold text-xl `}
+                                >
+                                    Check todo's
+                                </div>: null
+            }
+      </div>
+  )
 }
 
 export default App;
